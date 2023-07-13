3d3285b1224b03d2baa653dde9df7345f89c5b6a
md.vorobev <md.vorobev@i-teco.ru>
2023-07-13 14:53:04 +0300

    fix

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	config "link_shortener/internal/configs"
-	"link_shortener/internal/services"
+	"link_shortener/internal/data"
+	"link_shortener/internal/shortenurl"
+	"strings"
 
 	"log"
 	"net/http"
@@ -15,6 +19,35 @@ import (
 type Config struct {
 	ServerAddr string `env:"SERVER_ADDRESS"`
 	BaseURL    string `env:"BASE_URL"`
+}
+
+func HandleGetRequest(w http.ResponseWriter, r *http.Request) {
+	id := strings.TrimPrefix(r.URL.Path, "/")
+	originalURL, found := data.URLMap[id]
+	if found {
+		w.Header().Set("Content-Type", "text/plain")
+		w.Header().Set("Location", originalURL)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else {
+		http.Error(w, "Invalid URL", http.StatusBadRequest)
+	}
+}
+
+func HandlePostRequest(w http.ResponseWriter, r *http.Request) {
+	conf := config.GetConfig()
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	originalURL := string(body)
+	shortURL := shortenurl.Shortener(originalURL)
+	data.URLMap[shortURL] = originalURL
+
+	response := fmt.Sprintf("%s/%s", conf.BaseURL, shortURL)
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(response))
 }
 
 func main() {
@@ -32,7 +65,7 @@ func main() {
 	flag.StringVar(&conf.Address, "a", "localhost:8080", "адрес")
 	flag.StringVar(&conf.BaseURL, "b", "http://localhost:8080", "базовый URL")
 	flag.Parse()
-	r.Post("/", services.HandlePostRequest)
-	r.Get("/{id}", services.HandleGetRequest)
+	r.Post("/", HandlePostRequest)
+	r.Get("/{id}", HandleGetRequest)
 	log.Fatal(http.ListenAndServe(conf.Address, r))
 }
