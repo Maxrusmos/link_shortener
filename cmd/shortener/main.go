6e27932f5005f5a8ffd994e686d5b3bf6478343a
md.vorobev <md.vorobev@i-teco.ru>
2023-07-13 15:47:28 +0300

    add shortenerurlPack

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	config "link_shortener/internal/configs"
-	"link_shortener/internal/services"
+	"link_shortener/internal/shortenurl"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/go-chi/chi"
 )
@@ -14,57 +17,65 @@ import (
 var urlMap = make(map[string]string)
 var conf = config.GetConfig()
 
-// func handleGetRequest(w http.ResponseWriter, r *http.Request) {
-// 	id := strings.TrimPrefix(r.URL.Path, "/")
-// 	originalURL, found := urlMap[id]
-// 	if found {
-// 		w.Header().Set("Content-Type", "text/plain")
-// 		w.Header().Set("Location", originalURL)
-// 		w.WriteHeader(http.StatusTemporaryRedirect)
-// 	} else {
-// 		http.Error(w, "Invalid URL", http.StatusBadRequest)
-// 	}
-// }
-
-// func handlePostRequest(w http.ResponseWriter, r *http.Request) {
-// 	body, err := io.ReadAll(r.Body)
-// 	if err != nil {
-// 		http.Error(w, "Bad Request", http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	originalURL := string(body)
-// 	shortURL := shortenurl.Shortener(originalURL)
-// 	urlMap[shortURL] = originalURL
-
-// 	response := fmt.Sprintf("%s/%s", conf.BaseURL, shortURL)
-// 	w.Header().Set("Content-Type", "text/plain")
-// 	w.WriteHeader(http.StatusCreated)
-// 	w.Write([]byte(response))
-// }
-
-type Config struct {
-	ServerAddr string `env:"SERVER_ADDRESS"`
-	BaseURL    string `env:"BASE_URL"`
+func handleGetRequest(w http.ResponseWriter, r *http.Request) {
+	id := strings.TrimPrefix(r.URL.Path, "/")
+	originalURL, found := urlMap[id]
+	if found {
+		w.Header().Set("Content-Type", "text/plain")
+		w.Header().Set("Location", originalURL)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else {
+		http.Error(w, "Invalid URL", http.StatusBadRequest)
+	}
 }
 
-func main() {
-	var cfg Config
-	if os.Getenv("SERVER_ADDRESS") != "" {
-		cfg.ServerAddr = os.Getenv("SERVER_ADDRESS")
-		conf.Address = cfg.ServerAddr
+func handlePostRequest(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
 	}
-	if os.Getenv("BASE_URL") != "" {
-		cfg.BaseURL = os.Getenv("BASE_URL")
-		conf.BaseURL = cfg.BaseURL
+
+	originalURL := string(body)
+	shortURL := shortenurl.Shortener(originalURL)
+	urlMap[shortURL] = originalURL
+
+	response := fmt.Sprintf("%s/%s", conf.BaseURL, shortURL)
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(response))
+}
+
+// type Config struct {
+// 	ServerAddr string `env:"SERVER_ADDRESS"`
+// 	BaseURL    string `env:"BASE_URL"`
+// }
+
+func main() {
+	// var cfg Config
+	// if os.Getenv("SERVER_ADDRESS") != "" {
+	// 	cfg.ServerAddr = os.Getenv("SERVER_ADDRESS")
+	// 	conf.Address = cfg.ServerAddr
+	// }
+	// if os.Getenv("BASE_URL") != "" {
+	// 	cfg.BaseURL = os.Getenv("BASE_URL")
+	// 	conf.BaseURL = cfg.BaseURL
+	// }
+	if os.Getenv(conf.ServerAddrENV) != "" {
+		conf.ServerAddrENV = os.Getenv(conf.ServerAddrENV)
+		conf.Address = conf.ServerAddrENV
+	}
+	if os.Getenv(conf.BaseURLENV) != "" {
+		conf.BaseURLENV = os.Getenv(conf.BaseURLENV)
+		conf.BaseURL = conf.BaseURLENV
 	}
 
 	r := chi.NewRouter()
 	flag.StringVar(&conf.Address, "a", "localhost:8080", "HTTP server address")
 	flag.StringVar(&conf.BaseURL, "b", "http://localhost:8080", "Base address for shortened URL")
 	flag.Parse()
-	r.Get("/{id}", services.HandleGetRequest)
-	r.Post("/", services.HandlePostRequest)
+	r.Get("/{id}", handleGetRequest)
+	r.Post("/", handlePostRequest)
 
 	log.Fatal(http.ListenAndServe(conf.Address, r))
 }
--- internal/configs/config.go
+++ internal/configs/config.go
@@ -1,16 +1,39 @@
+// package config
+
+// type Config struct {
+// 	Address string
+// 	BaseURL string
+// }
+
+// func GetConfig() *Config {
+// 	address := "localhost:8080"
+// 	baseURL := "http://localhost:8080"
+
+// 	return &Config{
+// 		Address: address,
+// 		BaseURL: baseURL,
+// 	}
+// }
+
 package config
 
 type Config struct {
-	Address string
-	BaseURL string
+	Address       string
+	BaseURL       string
+	ServerAddrENV string `env:"SERVER_ADDRESS"`
+	BaseURLENV    string `env:"BASE_URL"`
 }
 
 func GetConfig() *Config {
 	address := "localhost:8080"
 	baseURL := "http://localhost:8080"
+	serveraddrEnv := "SERVER_ADDRESS"
+	baseurlEnv := "BASE_URL"
 
 	return &Config{
-		Address: address,
-		BaseURL: baseURL,
+		Address:       address,
+		BaseURL:       baseURL,
+		ServerAddrENV: serveraddrEnv,
+		BaseURLENV:    baseurlEnv,
 	}
 }
