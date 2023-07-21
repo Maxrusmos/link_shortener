cfd7d844f41fe7a80e6dec864d3064fe96378479
Maxim Vorobyev <vmaxim7777@gmail.com>
2023-07-21 11:12:25 +0300

    iter6

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,23 +3,22 @@ package main
 import (
 	"flag"
 	config "link_shortener/internal/configs"
-	"link_shortener/internal/middleware"
+
+	// "link_shortener/internal/middleware"
 	"link_shortener/internal/services"
 	"link_shortener/internal/storage"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/go-chi/chi"
-	"go.uber.org/zap"
 )
 
 func main() {
-	logger, err := zap.NewProduction()
-	if err != nil {
-		panic(err)
-	}
-	defer logger.Sync()
+	// logger, err := zap.NewProduction()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer logger.Sync()
 
 	conf := config.GetConfig()
 	r := chi.NewRouter()
@@ -36,18 +35,18 @@ func main() {
 		services.HandlePostRequest(w, r, storage, conf.BaseURL)
 	})
 
-	server := &http.Server{
-		Addr:         ":8080",
-		Handler:      middleware.LoggingMiddleware(logger, r),
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		IdleTimeout:  120 * time.Second,
-	}
+	// server := &http.Server{
+	// 	Addr:         ":8080",
+	// 	Handler:      middleware.LoggingMiddleware(logger, r),
+	// 	ReadTimeout:  5 * time.Second,
+	// 	WriteTimeout: 10 * time.Second,
+	// 	IdleTimeout:  120 * time.Second,
+	// }
 
-	logger.Info("server started")
-	if err := server.ListenAndServe(); err != nil {
-		logger.Error("server stopped", zap.Error(err))
-	}
+	// logger.Info("server started")
+	// if err := server.ListenAndServe(); err != nil {
+	// 	logger.Error("server stopped", zap.Error(err))
+	// }
 
 	log.Fatal(http.ListenAndServe(conf.Address, r))
 }
