d749e5da86311c70ac286399c0fe00f6b831642b
Maxim Vorobyev <vmaxim7777@gmail.com>
2023-07-21 10:54:30 +0300

    iter6

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,15 +3,24 @@ package main
 import (
 	"flag"
 	config "link_shortener/internal/configs"
+	"link_shortener/internal/loggerMiddleware"
 	"link_shortener/internal/services"
 	"link_shortener/internal/storage"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi"
+	"go.uber.org/zap"
 )
 
 func main() {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+
 	conf := config.GetConfig()
 	r := chi.NewRouter()
 	flag.StringVar(&conf.Address, "a", "localhost:8080", "HTTP server address")
@@ -26,6 +35,19 @@ func main() {
 	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
 		services.HandlePostRequest(w, r, storage, conf.BaseURL)
 	})
+
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      loggerMiddleware.LoggingMiddleware(logger, r),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	logger.Info("server started")
+	if err := server.ListenAndServe(); err != nil {
+		logger.Error("server stopped", zap.Error(err))
+	}
 
 	log.Fatal(http.ListenAndServe(conf.Address, r))
 }
