776092e037aac8b35ba769d246f547a275b020a7
md.vorobev <md.vorobev@i-teco.ru>
2023-08-14 02:38:59 +0300

    iter11

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -28,7 +28,7 @@ func main() {
 	r := chi.NewRouter()
 	flag.StringVar(&conf.Address, "a", "localhost:8080", "HTTP server address")
 	flag.StringVar(&conf.BaseURL, "b", "http://localhost:8080", "Base address for shortened URL")
-	flag.StringVar(&conf.FileStore, "f", "", "File storage")
+	flag.StringVar(&conf.FileStore, "f", "short-url-db.json", "File storage")
 	// short-url-db.json
 	flag.StringVar(&conf.DBConnect, "d", "", "db Connection String")
 	// user=postgres password=490Sutud dbname=link-shortener sslmode=disable
