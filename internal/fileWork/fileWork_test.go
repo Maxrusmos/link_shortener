d7b729aaa14cb894fba55a3a8063e6d862962d5a
md.vorobev <md.vorobev@i-teco.ru>
2023-08-15 17:06:55 +0300

    iter12

---
+++ internal/fileWork/fileWork_test.go
@@ -0,0 +1,64 @@
+package filework
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+)
+
+func TestWriteURLsToFile(t *testing.T) {
+	filename := "test.txt"
+	defer os.Remove(filename)
+
+	dataToWrite := JSONURLs{
+		ShortURL:  "short",
+		OriginURL: "http://example.com",
+	}
+
+	err := WriteURLsToFile(filename, dataToWrite)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	fileData, err := os.ReadFile(filename)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	var writtenData JSONURLs
+	err = json.Unmarshal(fileData, &writtenData)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	if writtenData.ShortURL != dataToWrite.ShortURL {
+		t.Errorf("Unexpected short URL: got %s, want %s", writtenData.ShortURL, dataToWrite.ShortURL)
+	}
+	if writtenData.OriginURL != dataToWrite.OriginURL {
+		t.Errorf("Unexpected origin URL: got %s, want %s", writtenData.OriginURL, dataToWrite.OriginURL)
+	}
+}
+
+func TestFindOriginURL(t *testing.T) {
+	filename := "test.txt"
+	defer os.Remove(filename)
+
+	dataToWrite := JSONURLs{
+		ShortURL:  "short",
+		OriginURL: "http://example.com",
+	}
+
+	err := WriteURLsToFile(filename, dataToWrite)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	originURL, err := FindOriginURL(filename, "short")
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	if originURL != dataToWrite.OriginURL {
+		t.Errorf("Unexpected origin URL: got %s, want %s", originURL, dataToWrite.OriginURL)
+	}
+}
