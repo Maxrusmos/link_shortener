ecf21297e259756eedca297f0ad87cd22bd97eab
md.vorobev <md.vorobev@i-teco.ru>
2023-08-29 12:05:28 +0300

    iter14

--- internal/services/services.go
+++ internal/services/services.go
@@ -43,11 +43,6 @@ func HandlePostRequest(w http.ResponseWriter, r *http.Request, storage storage.U
 	originalURL := strings.TrimSpace(string(body))
 	shortURL := shortenurl.Shortener(originalURL)
 	userID := cookieswork.GetUserID(r)
-	// if userID == "" {
-	// 	fmt.Println("userIDempty")
-	// 	w.WriteHeader(http.StatusUnauthorized)
-	// 	return
-	// }
 
 	mutex.Lock()
 	defer mutex.Unlock()
@@ -121,11 +116,6 @@ func ShortenHandler(w http.ResponseWriter, r *http.Request, storage storage.URLS
 		return
 	}
 	userID := cookieswork.GetUserID(r)
-	if userID == "" {
-		fmt.Println("userIDempty")
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
 	shortURL, err := storage.AddURLSH(url.URL, userID)
 	if err != nil {
 		http.Error(w, "Failed to add URL", http.StatusInternalServerError)
@@ -170,11 +160,6 @@ func HandleBatchShorten(w http.ResponseWriter, r *http.Request, storage storage.
 
 	var responses []BatchURLResponse
 	userID := cookieswork.GetUserID(r)
-	if userID == "" {
-		fmt.Println("userIDempty")
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
 	for _, req := range requests {
 		shortURL, err := storage.AddURLSH(req.OriginalURL, userID)
 		if err != nil {
@@ -200,15 +185,13 @@ func HandleBatchShorten(w http.ResponseWriter, r *http.Request, storage storage.
 }
 
 func UserUrlsHandler(w http.ResponseWriter, r *http.Request, storage storage.URLStorage) {
-	// cookieswork.SetAuthCookie(w, "user1")
 	w.Header().Set("Content-Type", "application/json")
 	userID := cookieswork.GetUserID(r)
 	fmt.Println(userID)
 	if userID == "" {
 		fmt.Println("userIDempty")
-		// w.WriteHeader(http.StatusUnauthorized)
-		// return
-		// w.WriteHeader(http.StatusNoContent)
+		w.WriteHeader(http.StatusUnauthorized)
+		return
 	}
 	jsonUrls, err := getUserUrls(userID, storage)
 	if err != nil {
@@ -223,7 +206,6 @@ func UserUrlsHandler(w http.ResponseWriter, r *http.Request, storage storage.URL
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(jsonUrls)
-
 }
 
 func getUserUrls(userID string, storage storage.URLStorage) ([]byte, error) {
